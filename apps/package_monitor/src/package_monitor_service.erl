b34e2b631bffd5f7bd06ad84c2d981a8d8c91b47
Andrew LaPointe <andrewrlapointe@gmail.com>
2024-10-10 15:42:02 -0600

    stuff

--- apps/package_monitor/src/package_monitor_service.erl
+++ apps/package_monitor/src/package_monitor_service.erl
@@ -2,26 +2,39 @@
 % The Package Monitor Service can trigger notifications by calling the notify/2 function from the Notification Service. Here’s how this part looks:
 
 -module(package_monitor_service).
--behaviour(gen_event).
+-behaviour(gen_server).
 
 %% API
--export([start_link/0, notify_status_change/2]).
--export([init/1, handle_event/2, terminate/2]).
+-export([start_link/0, update_db_record/2]).
+-export([init/1, handle_cast/2, terminate/2, code_change/3]).
 
 %% Client API
 start_link() ->
-    gen_event:start_link({local, ?MODULE}).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-%% Function to notify about a status change
-notify_status_change(PackageId, Status) ->
-    notification_service:notify(PackageId, Status).
+%% Update the database record with the provided data
+update_db_record(PackageId, Data) ->
+    gen_server:cast(?MODULE, {update_db_record, PackageId, Data}).
 
-%% gen_event callbacks (for potential events related to monitoring)
+%% gen_server callbacks
 init([]) ->
     {ok, #{}}.
 
-handle_event(_Event, State) ->
-    {ok, State}.
+handle_cast({update_db_record, PackageId, Data}, State) ->
+    %% Store data in Riak using the package ID as the key
+    case riak_kv:put(PackageId, Data) of
+        ok ->
+            %% Notify the Notification Service
+            notification_service:notify(PackageId, Data),
+            io:format("Package ~p updated successfully with data: ~p.~n", [PackageId, Data]),
+            {noreply, State};
+        {error, Reason} ->
+            io:format("Failed to update package ~p: ~p.~n", [PackageId, Reason]),
+            {noreply, State}
+    end.
 
 terminate(_Reason, _State) ->
-    ok. 
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
