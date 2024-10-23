b6dc42c84949665b0214970695525181084e9095
Andrew LaPointe <andrewrlapointe@gmail.com>
2024-10-23 14:27:25 -0600

    registration running locally

--- apps/registration/src/registration_sup.erl
+++ apps/registration/src/registration_sup.erl
@@ -7,29 +7,27 @@
 
 -behaviour(supervisor).
 
--export([start_link/0]).
+-export([start_link/0, init/1]).
 
--export([init/1]).
 
 -define(SERVER, ?MODULE).
 
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%% sup_flags() = #{strategy => strategy(),         % optional
-%%                 intensity => non_neg_integer(), % optional
-%%                 period => pos_integer()}        % optional
-%% child_spec() = #{id => child_id(),       % mandatory
-%%                  start => mfargs(),      % mandatory
-%%                  restart => restart(),   % optional
-%%                  shutdown => shutdown(), % optional
-%%                  type => worker(),       % optional
-%%                  modules => modules()}   % optional
+
 init([]) ->
     SupFlags = #{strategy => one_for_all,
-                 intensity => 0,
-                 period => 1},
-    ChildSpecs = [],
+                 intensity => 3,
+                 period => 5},
+    ChildSpecs = [
+                #{id => registration_app,            % Unique ID for this child
+          start => {registration_app, start_link, []}, % Start function
+          restart => permanent,                 % Always restart on failure
+          shutdown => 5000,                     % 5-second timeout for shutdown
+          type => worker,                       % Type: worker process
+          modules => [registration_app]}     % Modules this child uses
+        ],
     {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
