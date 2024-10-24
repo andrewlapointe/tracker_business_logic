872422415f1d3009fd4092c38d720835ef6671af
Andrew LaPointe <andrewrlapointe@gmail.com>
2024-10-23 19:11:53 -0600

    package_monitor works, notification service still inprogress and has been commented out temporarily

--- apps/package_monitor/src/package_monitor_sup.erl
+++ apps/package_monitor/src/package_monitor_sup.erl
@@ -16,20 +16,19 @@
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%% sup_flags() = #{strategy => strategy(),         % optional
-%%                 intensity => non_neg_integer(), % optional
-%%                 period => pos_integer()}        % optional
-%% child_spec() = #{id => child_id(),       % mandatory
-%%                  start => mfargs(),      % mandatory
-%%                  restart => restart(),   % optional
-%%                  shutdown => shutdown(), % optional
-%%                  type => worker(),       % optional
-%%                  modules => modules()}   % optional
+
 init([]) ->
     SupFlags = #{strategy => one_for_all,
-                 intensity => 0,
-                 period => 1},
-    ChildSpecs = [],
+                 intensity => 3,
+                 period => 5},
+    ChildSpecs = [
+                #{id => package_monitor_app,            % Unique ID for this child
+          start => {package_monitor_app, start_link, []}, % Start function
+          restart => permanent,                 % Always restart on failure
+          shutdown => 5000,                     % 5-second timeout for shutdown
+          type => worker,                       % Type: worker process
+          modules => [package_monitor_app]}     % Modules this child uses
+        ],
     {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
