7ce1f0ac417cc54cc12e5d68f2626337dc86ff05
Aidan Foushee <aidanfoushee@gmail.com>
2024-11-07 13:43:49 -0700

    added a function to generate unique package keys

--- apps/utils.erl
+++ apps/utils.erl
@@ -1,5 +1,5 @@
 -module(utils).
--export([riak_ip_address/0, port_number/0]).
+-export([riak_ip_address/0, port_number/0, generate_package_key/0]).
 
 riak_ip_address() ->
     % Change this to match the ip address of your riak droplet
@@ -7,3 +7,9 @@ riak_ip_address() ->
 
 port_number() ->
     "".
+
+generate_package_key() ->
+    UniqueID = erlang:unique_integer([monotonic, positive]),
+    Timestamp = calendar:datetime_to_gregorian_seconds(calendar:universal_time()),
+    Key = io_lib:format("pkg-~p-~p", [Timestamp, UniqueID]),
+    list_to_binary(Key).
