49f316b52b40589112a9a30add4be6af92f74065
Andrew LaPointe <andrewrlapointe@gmail.com>
2024-11-15 14:26:04 -0700

    working get/3 function in tracking service

--- apps/tracking/src/tracking_app.erl
+++ apps/tracking/src/tracking_app.erl
@@ -18,22 +18,49 @@ get_status(PackageId) ->
 
 %% gen_server callbacks
 init([]) ->
-    {ok, #{}}.
+    RiakHost = utils:riak_ip_address(),
+    RiakPort = utils:port_number(),
+    Bucket = <<"bucket">>,
+        case riakc_pb_socket:start_link(RiakHost, RiakPort) of
+        {ok, RiakPid} ->
+            {ok, #{
+                riak_pid => RiakPid,
+                bucket => Bucket
+            }};
+        {error, Reason} ->
+            {stop, Reason}
+    end.
 
 handle_call({get_status, PackageId}, _From, State) ->
     %% Fetch the package status from the data_interaction module
-    case data_interaction:get_package_status(PackageId) of
+    case get_package_status(PackageId, State) of
         {ok, Data} ->
-            %% Return the correct 3-tuple format: {reply, Reply, State}
-            {reply, {ok, Data}, State};
+            case catch binary_to_term(Data) of
+                Term when is_map(Term) orelse is_list(Term) ->
+                    {reply, {ok, Term}, State};
+                _Error ->
+                    {reply, {error, "Unable to decode data"}, State}
+            end;
         {error, not_found} ->
             {reply, {error, "Package not found"}, State};
         {error, Reason} ->
             {reply, {error, Reason}, State}
     end.
-    
 
-terminate(_Reason, _State) ->
+get_package_status(PackageId, State) ->
+    RiakPid = maps:get(riak_pid, State),
+    Bucket = maps:get(bucket, State),
+    BinaryKey = integer_to_binary(PackageId),
+    case riakc_pb_socket:get(RiakPid, Bucket, BinaryKey) of
+        {ok, Object} ->
+            {ok, riakc_obj:get_value(Object)};
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+terminate(_Reason, State) ->
+    RiakPid = maps:get(riak_pid, State),
+    riakc_pb_socket:stop(RiakPid),
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
