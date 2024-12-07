efc075c3e9404d9a5d80338715fe7fd21e69bb77
NoahFTW123 <noahjlarsen20@gmail.com>
2024-12-07 11:34:25 -0700

    test for handlers

--- src/registration_server.erl
+++ src/registration_server.erl
@@ -23,16 +23,20 @@ init([]) ->
     self() ! {connect_riak, RiakHost, RiakPort},
     {ok, #{bucket => Bucket, riak_pid => undefined}}.
 
-handle_call({register, PackageData}, _From, State) ->
-    PackageId = maps:get(package_id, PackageData),
-    case put(PackageId, PackageData, State) of
-        {reply, ok, State} ->
-            io:format("Package ~p registered successfully.~n", [PackageId]),
-            {reply, {ok, "Package registered"}, State};
+handle_call({register_package, BinaryData}, _From, State) ->
+    %% Process the binary data
+    %% Example: Parse or decode the binary data if needed
+    case parse_package_data(BinaryData) of
+        {ok, ParsedData} ->
+            %% Simulate storing the data or performing the required action
+            PackageKey = utils:generate_package_key(),
+            io:format("Package ~p registered with data: ~p~n", [PackageKey, ParsedData]),
+            {reply, {ok, "Package registered", PackageKey}, State};
         {error, Reason} ->
-            io:format("Failed to register package ~p: ~p~n", [PackageId, Reason]),
+            io:format("Failed to register package: ~p~n", [Reason]),
             {reply, {error, Reason}, State}
     end.
+
 
 handle_info({connect_riak, RiakHost, RiakPort}, State) ->
     case riakc_pb_socket:start_link(RiakHost, RiakPort) of
@@ -49,12 +53,10 @@ handle_info({connect_riak, RiakHost, RiakPort}, State) ->
 handle_info(_Msg, State) ->
     {noreply, State}.
 
-
-
 put(Key, Value, State) ->
     RiakPid = maps:get(riak_pid, State),
     Bucket = maps:get(bucket, State),
-    BinaryKey = integer_to_binary(Key), 
+    BinaryKey = Key, % Already in binary format from generate_package_key/0
     Object = riakc_obj:new(Bucket, BinaryKey, Value),
     case riakc_pb_socket:put(RiakPid, Object) of
         ok ->
@@ -65,8 +67,19 @@ put(Key, Value, State) ->
 
 terminate(_Reason, State) ->
     RiakPid = maps:get(riak_pid, State),
-    riakc_pb_socket:stop(RiakPid),
-    ok.
+    case RiakPid of
+        undefined -> ok;
+        _ -> riakc_pb_socket:stop(RiakPid)
+    end.
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+parse_package_data(BinaryData) ->
+    %% Example: Decode URL-encoded binary data into a map
+    try
+        Decoded = http_uri:parse_query(BinaryData),
+        {ok, maps:from_list(Decoded)}
+    catch
+        _:_ -> {error, invalid_data}
+    end.
