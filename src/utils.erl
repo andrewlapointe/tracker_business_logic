acee96140e0811e92052cda1bad31da32393b77c
Adam Tyson <tyson212144@gmail.com>
2024-12-09 14:08:52 -0700

    Env Var added

--- src/utils.erl
+++ src/utils.erl
@@ -2,12 +2,23 @@
 -export([riak_ip_address/0, port_number/0, generate_package_key/0]).
 
 riak_ip_address() ->
-    % Change this to match the ip address of your riak droplet
-    % "206.81.11.205".
-    "24.144.84.21".
+    case os:getenv("RIAK_IP_ADDRESS") of
+        undefined ->
+            % Default IP address if the environmental variable is not set
+            "24.144.84.21";
+        IP ->
+            IP
+    end.
 
 port_number() ->
-    8087.
+    case os:getenv("RIAK_PORT") of
+        undefined ->
+            % Default port number if the environmental variable is not set
+            8087;
+        PortString ->
+            % Convert the string to an integer
+            list_to_integer(PortString)
+    end.
 
 generate_package_key() ->
     UniqueID = erlang:unique_integer([monotonic, positive]),
